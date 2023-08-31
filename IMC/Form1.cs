a3de39078450fce5cc4c77f4dd5ea0f1a05ce81e
Lucas Raimundo <lucasraimundo2010@gmail.com>
2023-08-31 00:26:36 -0300

    logic works

--- IMC/Form1.cs
+++ IMC/Form1.cs
@@ -16,5 +16,36 @@ namespace IMC
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            float kg = float.Parse(this.textBox1.Text);
+            float met = float.Parse(this.textBox2.Text);
+            float imc =  kg / (met * met);
+
+            string imcText = imc.ToString();
+
+            label4.Text = "Seu IMC é de " + imcText;
+            label4.Visible = true;
+
+            if (imc < 18.5)
+            {
+                label5.Text = "Abaixo do peso";
+            }
+            else if (imc < 25)
+            {
+                label5.Text = "Peso normal";
+            }
+            else if (imc < 30)
+            {
+                label5.Text = "Sobrepeso";
+            }
+            else
+            {
+                label5.Text = "Obeso";
+            }
+
+            label5.Visible = true;
+        }
     }
 }
